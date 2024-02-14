9c05e2e16eed26da298358330f22e58b0b28642a
aryanando <aryanando232@gmail.com>
2024-02-14 21:55:35 +0700

    Git Init + Auth

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Api\AuthenticationController;
+use App\Http\Controllers\API\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['namespace' => 'Api', 'prefix' => 'v1'], function () {
+    Route::post('login', [AuthenticationController::class, 'store']);
+    Route::post('register', [RegisterController::class, 'store']);
+    Route::post('logout', [AuthenticationController::class, 'destroy'])->middleware('auth:api');
+});
+
+// Route::middleware('auth:api')->group( function () {
+//     Route::resource('products', ProductController::class);
+// });
