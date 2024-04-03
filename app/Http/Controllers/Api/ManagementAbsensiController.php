252a0952ca6e8fba4cba2f778b9be572662ff31a
Aryanando Mac <aryanando232@gmail.com>
2024-04-03 10:33:51 +0700

    Fixing absensi

--- app/Http/Controllers/Api/ManagementAbsensiController.php
+++ app/Http/Controllers/Api/ManagementAbsensiController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 // use App\Http\Controllers\API\BaseController;
+
+use App\Models\Absens;
 use App\Models\Shifts;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -17,16 +19,13 @@ class ManagementAbsensiController extends BaseController
     public function index()
     {
         $user = Auth::user();
-        $dataAbsensi = new Shifts();
+        $dataAbsensi = new Absens();
 
         $data = array();
         $data['absensi_hari_ini'] = $dataAbsensi->select('absens.id as absen_id', 'absens.check_in as absen_check_in', 'absens.check_out as absen_check_out', 'shifts.check_in as shift_check_in', 'shifts.check_out as shift_check_out', 'shifts.next_day', 'users.id as user_id', 'users.name as user_name')
-            ->join('user_shifts', 'user_shifts.shift_id', '=', 'shifts.idz')
-            ->join('absens', 'absens.shift_id', '=', 'shifts.id')
             ->join('users', 'users.id', '=', 'absens.user_id')
+            ->join('shifts', 'shifts.id', '=', 'absens.shift_id')
             ->where(DB::raw("CAST('" . Carbon::today()->toDateString() . "' AS DATE)"), '=', DB::raw('CAST(absens.created_at AS DATE)'))
-            ->where(DB::raw("CAST('" . Carbon::today()->toDateString() . "' AS DATE)"), '=', DB::raw('CAST(user_shifts.valid_date_start AS DATE)'))
-            ->orWhere(DB::raw("CAST('" . Carbon::today()->toDateString() . "' AS DATE)"), '=', DB::raw('CAST(user_shifts.valid_date_end AS DATE)'))
             ->get();
 
         return response()->json([
