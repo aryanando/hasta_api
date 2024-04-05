eafae1bd6a0fbe1c77077d71ef9be6220719de7f
Aryanando Mac <aryanando232@gmail.com>
2024-04-05 10:30:23 +0700

    Adding ID

--- app/Http/Controllers/Api/AbsensiController.php
+++ app/Http/Controllers/Api/AbsensiController.php
@@ -41,7 +41,7 @@ class AbsensiController extends BaseController
                 ], 200);
             } else {
                 $data['shift_hari_ini'] = $dataAbsensi->select(
-                    'shifts.id', 'shifts.shift_name', 'shifts.check_in', 'shifts.check_out', 'shifts.color', 'shifts.next_day', 'shifts.unit_id', 'user_shifts.valid_date_start', 'user_shifts.valid_date_end', 'user_shifts.user_id', 'user_shifts.shift_id', 'user_shifts.update_shift_id', 'user_shifts.id as user_shifts_id'
+                     'shifts.id', 'shifts.shift_name', 'shifts.check_in', 'shifts.check_out', 'shifts.color', 'shifts.next_day', 'shifts.unit_id', 'user_shifts.valid_date_start', 'user_shifts.valid_date_end', 'user_shifts.user_id', 'user_shifts.shift_id', 'user_shifts.update_shift_id', 'user_shifts.id as user_shifts_id'
                 )
                 ->join('user_shifts', 'user_shifts.shift_id', '=', 'shifts.id')
                 ->where('user_shifts.user_id', '=', $user['id'])
