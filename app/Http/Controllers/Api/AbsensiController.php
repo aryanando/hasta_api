be8ff2665b07c6df6c3502ad7760a25cf589a20e
Aryanando Mac <aryanando232@gmail.com>
2024-04-05 10:29:11 +0700

    Adding ID

--- app/Http/Controllers/Api/AbsensiController.php
+++ app/Http/Controllers/Api/AbsensiController.php
@@ -40,7 +40,9 @@ class AbsensiController extends BaseController
                     'data' => $data,
                 ], 200);
             } else {
-                $data['shift_hari_ini'] = $dataAbsensi->select('*')
+                $data['shift_hari_ini'] = $dataAbsensi->select(
+                    'shifts.id', 'shifts.shift_name', 'shifts.check_in', 'shifts.check_out', 'shifts.color', 'shifts.next_day', 'shifts.unit_id', 'user_shifts.valid_date_start', 'user_shifts.valid_date_end', 'user_shifts.user_id', 'user_shifts.shift_id', 'user_shifts.update_shift_id', 'user_shifts.id as user_shifts_id'
+                )
                 ->join('user_shifts', 'user_shifts.shift_id', '=', 'shifts.id')
                 ->where('user_shifts.user_id', '=', $user['id'])
                 ->where(DB::raw("CAST('" . Carbon::today()->toDateString() . "' AS DATE)"), '>=', DB::raw('CAST(user_shifts.valid_date_start AS DATE)'))
@@ -75,7 +77,7 @@ class AbsensiController extends BaseController
                         }
                     }else{
                         $data['absensi_hari_ini'] = $dataAbsensi->select(
-                            'shifts.id', 'shifts.shift_name', 'shifts.check_in', 'shifts.check_out', 'shifts.color', 'shifts.next_day', 'shifts.unit_id', 'user_shifts.valid_date_start', 'user_shifts.valid_date_end', 'user_shifts.user_id', 'user_shifts.shift_id', 'user_shifts.update_shift_id', 'user_shifts.id as user_shifts_id'
+                            '*'
                         )
                         ->join('user_shifts', 'user_shifts.shift_id', '=', 'shifts.id')
                         ->join('absens', 'absens.shift_id', '=', 'shifts.id')
