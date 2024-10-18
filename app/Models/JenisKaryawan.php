c289412a45523db68ece908557feeb7a442da5e2
Aryanando PC Kantor <aryanando232@gmail.com>
2024-10-18 07:49:23 +0700

    Adding type karyawan

---
+++ app/Models/JenisKaryawan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisKaryawan extends Model
+{
+    use HasFactory;
+}
